e4887d9cbeee7bc4e16c3bbbcbab96edfc035c87
Yuya Otsuka <otsuka.main@google.com>
2016-11-08 19:31:31 +0900

    fix<NavigationBar>: 仕様に従ってリンクを修正
    *「プロフィール」へのリンクを追加。
    *「アンケート」などの仕様にないページへのリンクを削除。

--- src/neeco/views/common/NavigationBar/index.jsx
+++ src/neeco/views/common/NavigationBar/index.jsx
@@ -27,13 +27,6 @@ export default ({
               <SubItem text="開催する" path="/new_event" onClick={onSelectItem} className={style.Icon} />
             </Menu>
           </Item>
-          <Item text="お仕事" className={style.TasksIcon} >
-            <Menu>
-              <SubItem text="探す" path="/tasks" className={style.Icon} />
-              <SubItem text="依頼する" path="/new_task" className={style.Icon} />
-              <SubItem text="募集する" path="/new_task" className={style.Icon} />
-            </Menu>
-          </Item>
           <Item text="ファイル" path="/files" className={style.ArchiveIcon} />
           <Item text="Wiki" className={style.FileIcon}>
             <Menu>
@@ -41,9 +34,7 @@ export default ({
               <SubItem text="作る" path="/new_wiki" className={style.Icon} />
             </Menu>
           </Item>
-          <Item text="アンケート" path="/survey" className={style.QuestionIcon} />
-          <Item text="外部リンク" path="/path" className={style.LinkIcon} />
-          <Item text="お問い合わせ" path="/" className={style.EnvelopeIcon} />
+          <Item text="プロフィール" path="/profile" className={style.TasksIcon} />
         </Menu>
       </nav>
     </ScrollArea>
