55216c61fb7027169a96bde85f2a4e3bd9945dad
Yuya Otsuka <otsuka.main@google.com>
2016-12-03 12:12:36 +0900

    refactor(main): モジュール管理方式の変更

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,41 @@
-import Authentication                  from "neeco/views/Authentication"
-import EventsView                      from "neeco/views/pages/EventsView"
-import FilesView                       from "neeco/views/pages/FilesView"
-import NewEventView                    from "neeco/views/pages/NewEventView"
-import PasswordSettingsView            from "neeco/views/pages/PasswordSettingsView"
-import SettingsView                    from "neeco/views/pages/SettingsView"
-import TopView                         from "neeco/views/pages/TopView"
-import React                           from "react"
-import {render}                        from "react-dom"
-import {Router, Route, browserHistory} from "react-router"
+var Authentication                  = require("neeco/views/Authentication")
+var EventsView                      = require("neeco/views/pages/EventsView")
+var FilesView                       = require("neeco/views/pages/FilesView")
+var NewEventView                    = require("neeco/views/pages/NewEventView")
+var PasswordSettingsView            = require("neeco/views/pages/PasswordSettingsView")
+var SettingsView                    = require("neeco/views/pages/SettingsView")
+var TopView                         = require("neeco/views/pages/TopView")
+var React                           = require("react")
+var {render}                        = require("react-dom")
+var {Router, Route, browserHistory} = require("react-router")
 
 render(
     <Router history={browserHistory}>
       <Route component={Authentication}>
-        <Route path="/"                  component={TopView} />
-        <Route path="/events"            component={EventsView} />
-        <Route path="/files"             component={FilesView} />
-        <Route path="/new_event"         component={NewEventView} />
-        <Route path="/settings"          component={SettingsView} />
-        <Route path="/settings/password" component={PasswordSettingsView} />
+        <Route
+          path="/"
+          component={TopView}
+        />
+        <Route
+          path="/events"
+          component={EventsView}
+        />
+        <Route
+          path="/files"
+          component={FilesView}
+        />
+        <Route
+          path="/new_event"
+          component={NewEventView}
+        />
+        <Route
+          path="/settings"
+          component={SettingsView}
+        />
+        <Route
+          path="/settings/password"
+          component={PasswordSettingsView}
+        />
       </Route>
     </Router>,
     document.querySelector(".root")
