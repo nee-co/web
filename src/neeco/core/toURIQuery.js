d0632578c466ff5819add395e2ccdd5eaf7abce5
Yuya Otsuka <otsuka.main@google.com>
2016-12-14 21:41:43 +0900

    fix: シングルクォートが注入されるバグを修正

--- src/neeco/core/toURIQuery.js
+++ src/neeco/core/toURIQuery.js
@@ -2,7 +2,7 @@ module.exports = (a) => {
     var b = ""
 
     for (var i in a)
-        b += encodeURIComponent(i) + "='" + encodeURIComponent(a[i]) + "'&"
+        b += encodeURIComponent(i) + "=" + encodeURIComponent(a[i]) + "&"
 
     return b.slice(0, -1)
 }
